6c50e33667f2b5cf71b62c6744445395f3ab94ac
sohail-ahamed-qureshi <sohailqureshi82@gmail.com>
2021-05-29 11:52:03 +0530

    [sohail] Add: welcome message

---
+++ CalculatorClass/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CalculatorClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome Message for Calculator class");
+        }
+    }
+}
