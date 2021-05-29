73ab5615cc4a69cd835d7c34cdf033dbcce4e5c7
sohail-ahamed-qureshi <sohailqureshi82@gmail.com>
2021-05-29 12:18:04 +0530

    [sohail] Add: multilevel abstract

--- CalculatorClass/Program.cs
+++ CalculatorClass/Program.cs
@@ -2,11 +2,41 @@
 
 namespace CalculatorClass
 {
+    abstract class Calculator
+    {
+        public abstract void Mul( int firstNum, int secondNum);
+    }
+
+    abstract class Multiplication : Calculator
+    {
+        public override void Mul(int firstNum, int secondNum)
+        {
+            int result = firstNum * secondNum;
+            Console.WriteLine("abstract from Calculator class");
+            Console.WriteLine($"Multiplication of {firstNum} * {secondNum} = {result}");            
+        }
+
+        public abstract void Div(int firstNum, int secondNum);
+    }
+
+    class Division : Multiplication
+    {
+        public override void Div(int firstNum, int secondNum)
+        {
+            int result = firstNum / secondNum;
+            Console.WriteLine("abstract from Multiplication class");
+            Console.WriteLine($"Division of {firstNum} / {secondNum} = {result}");
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome Message for Calculator class");
+            Division division = new Division();
+            division.Mul(10, 5);
+            division.Div(5, 10);
         }
     }
 }
